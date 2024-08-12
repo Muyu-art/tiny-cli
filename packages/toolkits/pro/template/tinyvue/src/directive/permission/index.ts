321aa98cfc2598536491116d1baebe10097672dc
fmm <1442620801@qq.com>
2024-08-12 14:53:10 +0800

    fix: 修复role权限缓存问题

--- packages/toolkits/pro/template/tinyvue/src/directive/permission/index.ts
+++ packages/toolkits/pro/template/tinyvue/src/directive/permission/index.ts
@@ -1,27 +1,31 @@
 import {useUserStore} from '@/store';
 import {getRoleInfo} from "@/api/role";
 
+let permissionList = [];
+const EXPIRE = 60 * 1000;
+async function setPermissionList(roleId: number){
+  const {data} = await getRoleInfo(roleId)
+  const permissions = data.permission;
+  for (let i = 0; i < permissions.length; i += 1) {
+    permissionList.push(permissions[i].name)
+  }
+}
 async function checkPermission(el: any, binding: any) {
   const {value} = binding;
   // 获取role的permission
   const userStore = useUserStore();
   const {roleId} = userStore
-  let permissionList = [] as any;
-  if(localStorage.getItem(`role-permission:${roleId}`)){
-    permissionList = JSON.parse(localStorage.getItem(`role-permission:${roleId}`))
-  }else{
-    const {data} = await getRoleInfo(roleId)
-    const permissions = data.permission;
-    for (let i = 0; i < permissions.length; i += 1) {
-      permissionList.push(permissions[i].name)
-    }
-    localStorage.setItem(`role-permission:${roleId}`,JSON.stringify(permissionList));
+  if (!permissionList.length){
+    setPermissionList(roleId)
+    setTimeout(()=>{
+      permissionList = [];
+      setPermissionList(roleId)
+    }, EXPIRE)
   }
   const hasPermission = permissionList.includes(value) || permissionList.includes('*');
   if (!hasPermission && el.parentNode) {
     el.parentNode.removeChild(el)
   }
-
 }
 
 export default {
