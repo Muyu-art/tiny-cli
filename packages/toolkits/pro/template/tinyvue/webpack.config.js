344bb0df04f719b0ff5f909189167223fe8d365f
fmm <1442620801@qq.com>
2024-10-05 00:10:35 +0800

    fix: vue关闭overlay

--- packages/toolkits/pro/template/tinyvue/webpack.config.js
+++ packages/toolkits/pro/template/tinyvue/webpack.config.js
@@ -1,30 +1,30 @@
-const {resolve} = require('path');
-const { VueLoaderPlugin } = require('vue-loader')
+const { resolve } = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {DefinePlugin} = require('webpack');
-const {configDotenv,parse} = require('dotenv');
+const { DefinePlugin } = require('webpack');
+const { configDotenv, parse } = require('dotenv');
 configDotenv({
-  path:'./.env'
-})
+  path: './.env',
+});
 module.exports = {
   mode: 'production',
   entry: {
-    main: './src/main.ts'
+    main: './src/main.ts',
   },
   output: {
     path: resolve(__dirname, 'dist'), // 打包后的文件输出的目录
     filename: `js/[name]_[chunkhash:8].js`, // 设置打包后的 js 文件名，如果在文件名前增加文件路径，会将打包后的 js 文件放在指定的文件夹下
-    publicPath: '/'
+    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        loader: 'vue-loader',
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(ts)$/,
@@ -33,13 +33,12 @@ module.exports = {
             loader: 'ts-loader',
             options: {
               appendTsSuffixTo: [/\.vue$/],
-              transpileOnly: true
+              transpileOnly: true,
             },
           },
           {
             loader: 'import-meta-loader',
-            options: {
-            }
+            options: {},
           },
         ],
         exclude: /node_modules/,
@@ -53,54 +52,54 @@ module.exports = {
           {
             loader: 'style-resources-loader',
             options: {
-                patterns: resolve(__dirname,'src/assets/style/breakpoint.less')
-            }
-          }
+              patterns: resolve(__dirname, 'src/assets/style/breakpoint.less'),
+            },
+          },
         ],
       },
       {
         test: /\.m?js/,
         resolve: {
-          fullySpecified: false
-        }
-      },
-      {
-        test:/.(png|jpg|jpeg|gif|svg)$/, // 匹配图片文件
-        type: "asset", // type选择asset
-        parser: {
-          dataUrlCondition: {
-            maxSize: 10 * 1024, // 小于10kb转base64位
-          }
-        },
-        generator:{
-          filename:'static/images/[name].[contenthash:8][ext]', // 文件输出目录和命名
+          fullySpecified: false,
         },
       },
       {
-        test:/.(woff2?|eot|ttf|otf)$/, // 匹配字体图标文件
-        type: "asset", // type选择asset
+        test: /.(png|jpg|jpeg|gif|svg)$/, // 匹配图片文件
+        type: 'asset', // type选择asset
         parser: {
           dataUrlCondition: {
             maxSize: 10 * 1024, // 小于10kb转base64位
-          }
+          },
         },
-        generator:{
-          filename:'static/fonts/[name].[contenthash:8][ext]', // 文件输出目录和命名
+        generator: {
+          filename: 'static/images/[name].[contenthash:8][ext]', // 文件输出目录和命名
         },
       },
       {
-        test:/.(mp4|webm|ogg|mp3|wav|flac|aac)$/, // 匹配媒体文件
-        type: "asset", // type选择asset
+        test: /.(woff2?|eot|ttf|otf)$/, // 匹配字体图标文件
+        type: 'asset', // type选择asset
         parser: {
           dataUrlCondition: {
             maxSize: 10 * 1024, // 小于10kb转base64位
-          }
+          },
         },
-        generator:{
-          filename:'static/media/[name].[contenthash:8][ext]', // 文件输出目录和命名
+        generator: {
+          filename: 'static/fonts/[name].[contenthash:8][ext]', // 文件输出目录和命名
         },
       },
-    ]
+      {
+        test: /.(mp4|webm|ogg|mp3|wav|flac|aac)$/, // 匹配媒体文件
+        type: 'asset', // type选择asset
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024, // 小于10kb转base64位
+          },
+        },
+        generator: {
+          filename: 'static/media/[name].[contenthash:8][ext]', // 文件输出目录和命名
+        },
+      },
+    ],
   },
   plugins: [
     new VueLoaderPlugin(),
@@ -115,8 +114,8 @@ module.exports = {
         preserveLineBreaks: false,
         minifyCSS: true,
         minifyJS: true,
-        removeComments: false
-      }
+        removeComments: false,
+      },
     }),
     new DefinePlugin({
       'process.env': JSON.stringify({ ...process.env }),
@@ -126,17 +125,18 @@ module.exports = {
       'import.meta.env.VITE_SERVER_HOST': '"http://127.0.0.1:3000"',
       'import.meta.env.VITE_MOCK_HOST': '"http://127.0.0.1:8848"',
       'import.meta.env.VITE_USE_MOCK': 'false',
-      'import.meta.env.VITE_MOCK_IGNORE': '"/api/user/userInfo,/api/user/login,/api/user/register,/api/employee/getEmployee"',
+      'import.meta.env.VITE_MOCK_IGNORE':
+        '"/api/user/userInfo,/api/user/login,/api/user/register,/api/employee/getEmployee"',
       'import.meta.env.VITE_MOCK_SERVER_HOST': '"/mock"',
-      BUILD_TOOLS: "'WEBPACK'",
-    })
+      'BUILD_TOOLS': "'WEBPACK'",
+    }),
   ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
       'assets': resolve(__dirname, 'src/assets'),
       'vue-i18n$': 'vue-i18n/dist/vue-i18n.esm-bundler.js',
-      'vue$': 'vue/dist/vue.esm-bundler.js'
+      'vue$': 'vue/dist/vue.esm-bundler.js',
     },
     extensions: ['.ts', '.js', '.vue'],
   },
@@ -145,23 +145,26 @@ module.exports = {
   },
   devServer: {
     historyApiFallback: true,
-    proxy:[
+    proxy: [
       {
         context: [process.env.VITE_BASE_API],
         target: process.env.VITE_SERVER_HOST,
         changeOrigin: true,
         pathRewrite: {
-          '^/api': ''
-        }
+          '^/api': '',
+        },
       },
       {
         context: [process.env.VITE_MOCK_SERVER_HOST],
         target: process.env.VITE_MOCK_HOST,
         changeOrigin: true,
-        pathRewrite:{
-          '^/mock': ''
-        }
-      }
-    ]
-  }
-}
+        pathRewrite: {
+          '^/mock': '',
+        },
+      },
+    ],
+    client: {
+      overlay: false,
+    },
+  },
+};
